1e8268d1ddd111b75f579b2fcc21ca2414bbbd54
cep104 <candicelizabethpeters@gmail.com>
2020-02-26 17:02:10 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,22 @@ class SessionsController < ApplicationController
         #if you want to delete the whole session use session.clear 
         redirect_to '/'
     end
+
+    def new 
+    end
+
+    def create 
+        # does the user exist in the system?
+        @user = User.find_by(username: params[:user][:username])
+        # did we find some one is it the right password
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to @user
+        else
+            flash[:error] = "Sorry wrong username or password"
+            redirect_to login_path
+        end
+
+        
+    end
 end
