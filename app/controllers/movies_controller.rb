6ca64eb8e61d36a870fe08bc9961fef236c56a0b
cep104 <candicelizabethpeters@gmail.com>
2020-02-26 18:20:42 -0600

    Almost done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+    def new 
+        @movie = Movie.new
+        @movie.build_genre
+        @movie.build_director
+        #wont need it in edit so putting it in new instead of form
+    end
+
+    def create 
+    @movie = Movie.new(movie_params)
+        @movie.user_id = session[:user_id]
+        if @movie.save!
+            redirect_to @movie 
+        else
+            render :new
+        end
+    end
+
+    private 
+    def movie_params 
+        params.require(:movie).permit(:title, :description, :genre_id, :director_id, genre_attributes:[:name], director_attributes: [:name] )
+    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,7 @@ class Movie < ApplicationRecord
   belongs_to :director
   has_many :reviews
   has_many :users, through: :reviews #people who have reviewed it
+
+  accepts_nested_attributes_for :genre
+  accepts_nested_attributes_for :director
 end
